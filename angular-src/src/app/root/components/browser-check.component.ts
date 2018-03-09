be48703b86cbc995fb0f729801d88be5151034d7
Syed Kazmi <syed.kazmi@kmandt.com>
2018-03-09 11:06:31 +0000

    added a browser check route to block IE

---
+++ angular-src/src/app/root/components/browser-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browser-check',
+  templateUrl: './browser-check.component.html',
+  styleUrls: ['./browser-check.component.css']
+})
+export class BrowserCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
